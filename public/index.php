70f0e80825c0dab73c3448529027ae51dfb91808
gvillarroel9 <gvillarroel9@gmail.com>
2020-03-17 12:05:07 -0400

    INDEX CORS

--- public/index.php
+++ public/index.php
@@ -8,12 +8,12 @@
  */
 
 define('LARAVEL_START', microtime(true));
-/*
+
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
- */
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
